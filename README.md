872e8a7b242dc15ae1d8e3076ab75bd8bd714d9c
ss <ss@hfuusec.cn>
2017-08-24 23:01:23 +0800

    adddata

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
+# 说明
 根据日期写日记或者总结每天学的事情，可以是流水账，可以是其他乱七八糟的东西，但是一定要根据时间（TIME），然后模仿hexo的目录结构（要比他简单的多很多）。
 
-使用方法 npm install hfuuss -g
-hfuuss new //生成今天的文章，md格式 hfuuss g //生成gitpage支持的格式 hfuuss d //部署到gitpage上面。
+# 使用方法 
+* `npm install sblog -g`    
+* `sblogsblog new //生成今天的文章，md格式`     
+* `sblog g //生成gitpage支持的格式`   
+* `hfuuss d //部署到gitpage上面。`  
+*  。。。
 
-markdown插件
-cli机械
-调用git
+markdown插件   
+cli机械  
+调用git  
 读取git配置文件，或者调用系统命令
+# 运行项目方法
+1、 `git clone git@github.com:hfuuss/hfuuss-cli.git`   
+2、 `cd hfuuss-cli`   
+3、 `npm install . -g` #安装到全局   
+4、 `sblog -h`
--- bin/main.js
+++ bin/main.js
@@ -1,16 +1,60 @@
 #!/usr/bin/env node
+/**
+ * Created by biezhi on 2017/8/11.
+ */
+var cmd     = require('commander');
+var appInfo = require('../package.json');
+var fs      = require('fs');
+var path    = require('path');
 
-var program = require('commander');
-program
- .version('0.1.0')
- .option('-p, --peppers', 'Add peppers')
- .option('-P, --pineapple', 'Add pineapple')
- .option('-b, --bbq-sauce', 'Add bbq sauce')
- .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
- .parse(process.argv);
+var animals = fs.readFileSync(path.join(__dirname, '../data/animals.txt')).toString()
+                .split('===============++++SEPERATOR++++====================\n');
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+var jokes  = fs.readFileSync(path.join(__dirname, '../data/jokes.txt')).toString().split('%\n');
+var quotes  = fs.readFileSync(path.join(__dirname, '../data/quotes.txt')).toString().split('%\n');
+var tang300 = fs.readFileSync(path.join(__dirname, '../data/tang300.txt')).toString().split('%\n');
+var song100 = fs.readFileSync(path.join(__dirname, '../data/song100.txt')).toString().split('%\n');
+
+/**
+ * 返回一个随机的动物ascii
+ *
+ * @returns {*}
+ */
+function randomAnimal() {
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
+/**
+ * 根据类型返回名言或段子
+ *
+ * @param type
+ * @returns {string}
+ */
+function prefix(type) {
+    switch (type) {
+        case 'quotes':
+            return quotes[Math.floor(Math.random() * quotes.length)];
+        case 'jokes':
+            return jokes[Math.floor(Math.random() * jokes.length)];
+        case 'tang':
+            return tang300[Math.floor(Math.random() * tang300.length)];
+        case 'song':
+            return song100[Math.floor(Math.random() * song100.length)];
+        default:
+            return tang300[Math.floor(Math.random() * tang300.length)];
+    }
+}
+
+cmd
+    .version(appInfo.version)
+    .option('-i, --index <n>', 'ascii art index, default is random', -1, parseInt)
+    .option('-t, --type <value>', '[quotes|jokes|tang|song]', 'quotes', /^(quotes|jokes|tang|song)$/i)
+    .on('--help', function(){
+        console.log('\t' + appInfo.repository.url);
+    }).parse(process.argv);
+
+var animal = cmd.index === -1 ? randomAnimal() : animals[cmd.index];
+
+console.log(prefix(cmd.type));
+
+console.log(animal);
